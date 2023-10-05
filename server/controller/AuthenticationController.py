19033e2f0821e8dd2f9b713edc495dd1bceb04ee
Mario Ruci <marioruci15@gmail.com>
2023-10-05 20:26:12 +0300

    fetching data from google using the access token

--- server/controller/AuthenticationController.py
+++ server/controller/AuthenticationController.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, request, jsonify
 from model import AuthenticationModel
 
@@ -17,23 +18,56 @@ class AuthenticationController:
 		self.blueprint.add_url_rule('/googleRegistration', view_func=self.googleRegistration, methods=['POST'])
 		self.blueprint.add_url_rule('/registration', view_func=self.registration, methods=['POST'])
 	
+	def getGoogleUserProfile(self, data):
+		try:
+			access_token = data['access_token']
+			response = requests.get('https://www.googleapis.com/oauth2/v3/userinfo', 
+				headers={'Authorization': 'Bearer ' + access_token})
+
+			if response.status_code == 200:
+				user_info = response.json()
+				return user_info
+			else:
+				print('failed to fetch google user profile')
+				return None
+		except Exception as e:
+			print({"error": str(e)})
+
+		return None
+
 	def googleLogin(self):
 		response = getResponse()
-		""" data = request.get_json()
+		data = request.get_json()
 		if not data:
 			response['error'] = 'Invalid input'
 			return jsonify(response)
 		
-		recipes = self.model.create(data['currentRecipes'], data['inventory'])
+		google_user_info = self.getGoogleUserProfile(data)
+		if not google_user_info:
+			response['error'] = 'Could not retrieve google user profile'
+			return jsonify(response)
 		
-		if recipes:
-			response['status'] ='ok'
-			response['data'] = recipes """
-
+		user = self.model.getUser(google_user_info['email'], False)
+		if not user:
+			response['error'] = 'The user is not registered with us. Please register first.'
+			return jsonify(response)
+		
+		""" Invoke to login logic here...s """
 		return jsonify(response)
 	
 	def login(self):
 		response = getResponse()
+		data = request.get_json()
+		if not data:
+			response['error'] = 'Inavlid input'
+			return jsonify(response)
+		
+		user = self.model.getUser(data['email'], False)
+		if not user:
+			response['error'] = 'The user is not registered with us. Please register first.'
+			return jsonify(response)
+			
+		""" Check password and the invoke the login logic """
 		return jsonify(response)
 	
 	def isLogged(self):
--- server/model/AuthenticationModel.py
+++ server/model/AuthenticationModel.py
@@ -1,15 +1,47 @@
 from db import db
+from sqlalchemy import Enum
 from sqlalchemy.exc import IntegrityError
 
-""" class Bookmarks(db.Model):
-    __tablename__ = 'Bookmarks'
+class Users(db.Model):
+	__tablename__ = 'Users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    name = db.Column(db.Text())
-    description = db.Column(db.Text(), default=None)
-    execution = db.Column(db.Text(), default=None)
-    ingredients = db.Column(db.Text(), default=None) """
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.Text())
+	surname = db.Column(db.Text())
+	email = db.Column(db.Text())
+	accountType = db.Column(Enum('google', 'normal', name='account_types'))
+
+	def as_dict(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'surname': self.surname,
+			'email': self.email,
+			'accountType': self.accountType
+		}
 
 class AuthenticationModel:
-    pass
+	def __init__(self) -> None:
+		pass
+	pass
+
+	def getUser(self, email, public=False):
+		if not email:
+			return False
+		
+		user = Users.query.with_entities(
+            Users.id if(not public) else None,
+			Users.name,
+			Users.surname,
+			Users.email,
+			Users.accountType,
+        ).filter(
+            Users.email == email
+        ).first()
+
+		if not user:
+			return None
+		
+		return user.as_dict()
+
+		
